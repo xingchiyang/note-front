47e382187dea7cd158bb5cdb2997ec12f9011b51
yangbian <yangbian@broada.com>
2017-04-20 21:21:18 +0800

    阅读密码

--- js/requestUtil.js
+++ js/requestUtil.js
@@ -18,6 +18,26 @@ function doPost(url, data, successCallback, errorCallback){
 	});
 }
 
+function doPostNoJson(url, data, successCallback, errorCallback){
+	$.ajax({
+		  type: 'POST',
+		  url: url,
+		  data: data,
+		  success: function(data){
+			if(successCallback != null)
+				successCallback(data);
+		  },
+		  error: function (XMLHttpRequest, textStatus, errorThrown){
+			if(XMLHttpRequest.responseJSON.message == "请先登录"){
+				window.top.location.href = "/login.html";
+			}
+			if(errorCallback != null)
+				errorCallback(XMLHttpRequest, textStatus, errorThrown);
+		  },
+		  contentType: "application/json;charset=utf-8"
+	});
+}
+
 function doGet(url, successCallback, errorCallback){
 	$.ajax({
 		  type: 'GET',
@@ -35,4 +55,10 @@ function doGet(url, successCallback, errorCallback){
 		  },
 		  contentType: "application/json;charset=utf-8"
 	});
+}
+
+function getQueryString(name){
+	 var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+	 var r = window.location.search.substr(1).match(reg);
+	 if(r!=null)return  unescape(r[2]); return null;
 }
